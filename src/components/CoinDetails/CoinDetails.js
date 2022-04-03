07208569baef02906bd932da44a9a578dcee124f
frsiam <siam.frs@gmail.com>
2022-04-03 12:27:28 +0600

    spinner added when page load the data

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
 const CoinDetails = () => {
     const {id} = useParams()
     const [coin, setCoin] = useState({})
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         const url = `https://api.coingecko.com/api/v3/coins/${id}`
         fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
             setCoin(data)
+            setLoading(false)
         })
     },[id])
     
     return (
-        <div className='h-{100vh} px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+        <>
+        
+        {loading ? (<Spinner></Spinner>) : (<div className='h-{100vh} px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
             <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 content-center'>
                 <div className='order-2 md:order-1'>
                     <h1 className='text-3xl'>General Info:</h1>
@@ -36,7 +42,9 @@ const CoinDetails = () => {
                     <img src={coin.image?.large} alt="" />
                 </div>
             </div>
-        </div>
+        </div>)}
+
+        </>
     );
 };
 
