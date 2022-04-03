5b5addfd7660396df39859d15b2ddcbb19d027b7
frsiam <siam.frs@gmail.com>
2022-04-03 12:40:58 +0600

    update some responsive issue and checked done

--- src/components/Contact/BdAddress.js
+++ src/components/Contact/BdAddress.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const BdAddress = () => {
   return (
-    <div className='border p-8 bg-blue-100 w-full'>
-      <div className='flex justify-between items-center'>
+    <div className='border p-8 bg-blue-100 w-100 md:w-full'>
+      <div className='flex flex-col md:flex-row justify-between items-center'>
         <div>
           <h1>Street: 17/4 Dhanmondi.</h1>
           <h1>City: Dhaka</h1>
